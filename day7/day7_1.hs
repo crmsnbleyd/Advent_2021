9b59b00c6c3e4d643ea7a1891d2a7f6008a41f52
arnav <arnav.jose@gmail.com>
2021-12-10 16:40:36 +0530

    day 7

--- day7/day7_1.hs
+++
@@ -1,9 +0,0 @@
-input = []
-
-diff op m = map (\ e -> op (abs $ e - m))
-
-solve op l = minimum [sum $ diff op c l | c <- [minimum l .. maximum l]]
-
-main = do
-  print $ solve id input
-  print $ solve (\x -> (x + 1) * x `div` 2) input
