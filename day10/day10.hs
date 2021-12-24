3900ad6f8a65cd80a924d45c980609d5683d8f1a
arnav <arnav.jose@gmail.com>
2021-12-24 13:00:01 +0530

    day 10 part 2

--- day10/day10.hs
+++ day10/day10.hs
@@ -1,3 +1,4 @@
+import Data.List(sort, foldl')
 match :: [Char] -> [Char] -> Int
 match (x:xs) stack
   | x `elem` "([{<" = match xs (x:stack)
@@ -9,9 +10,26 @@ match (x:xs) stack
   where (s:t) = stack
 match [] _ = 0
 
+complete :: [Char] -> [Char] -> Int
+complete (x:xs) stack
+  | x `elem` "([{<" = complete xs (x:stack)
+  | x == ')' && stack /= [] && s == '(' = complete xs t
+  | x == ']' && stack /= [] && s == '[' = complete xs t
+  | x == '}' && stack /= [] && s == '{' = complete xs t
+  | x == '>' && stack /= [] && s == '<' = complete xs t
+  | otherwise = error "illegal character"
+  where (s:t) = stack
+
+complete [] stack = foldl' (\acc ch -> acc*5 + func ch) 0 stack
+  where func '(' = 1
+        func '[' = 2
+        func '{' = 3
+        func '<' = 4
+        func _ = 0
+
 main :: IO()
 main = do
   inp <- readFile "10.txt"
-  -- print . (`match` []) . head . lines $ inp
-  print . sum . map (`match` []) . lines $ inp
-
+  print . foldl' (\acc str -> acc + match str []) 0 . lines $ inp
+  let completeLines =  filter (\str -> match str [] == 0) . lines $ inp
+  print . (!! (length completeLines `div` 2)) . sort . map (`complete` []) $ completeLines
